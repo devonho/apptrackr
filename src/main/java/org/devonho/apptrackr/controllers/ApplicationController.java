7474695f5cd8ff544d6c06fa0b0c3200664a0753
Devon Ho <devonho@symplesys.com>
2024-12-20 07:27:08 +0800

    Added JSP

--- src/main/java/org/devonho/apptrackr/controllers/ApplicationController.java
+++ src/main/java/org/devonho/apptrackr/controllers/ApplicationController.java
@@ -1,6 +1,7 @@
 package org.devonho.apptrackr.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -61,4 +62,5 @@ public class ApplicationController {
         return null;
       }      
     }
+   
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,7 +1,14 @@
 <html>
   <header>
     <link rel='stylesheet' href='/webjars/bootstrap/3.1.0/css/bootstrap.min.css'>
-    <h1>Hello</h1>
+    <h1>
+      <%
+      <%
+      import org.devonho.apptrackr.Foo;
+      out.println("Hello there! " + Foo.getFoo() );
+
+      %></h1>
+    
   </header>
   <body>
 
