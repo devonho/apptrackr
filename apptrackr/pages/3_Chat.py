df4f7757137b1ea443d3bc3b644c8fc6656b688b
Devon Ho <devonho@symplesys.com>
2025-01-03 14:49:49 +0800

    Fixed JD & CV selection in chat

--- apptrackr/pages/3_Chat.py
+++ apptrackr/pages/3_Chat.py
@@ -10,18 +10,14 @@ else:
     userhash = Configuration.getUserHash(st.session_state['username'])
     cvs = [ r for r in DB.retrieveRecords(Resume, userhash)]
     jds = [ r for r in DB.retrieveRecords(JobDescription, userhash)]
-    cv = cvs[0].content
-    jd = jds[0].content
 
-    st.title("CV Bot")
-
-    
+    st.title("CV Bot")   
     st.selectbox("CV", [r.title for r in cvs], key="w_cv_select")
     st.selectbox("JD", [r.role_title for r in jds], key="w_jd_select")
+    cv = [r for r in cvs if r.title == st.session_state.w_cv_select][0].content
+    jd = [r for r in jds if r.role_title == st.session_state.w_jd_select][0].content
 
-    #@st.dialog("Chat", width="large")
-    #def dialog():
-    #if "messages" not in st.session_state:
+
     st.session_state.messages = [{"role": "user", "content": f"Resume:\n {cv} "}, {"role": "user", "content": f"Job Description\n {jd}"}]
 
     for message in st.session_state.messages[2:]:
@@ -37,5 +33,3 @@ else:
         with st.chat_message("assistant"):
             st.markdown(response)
         st.session_state.messages.append({"role": "assistant", "content": response})
-
-    #st.button("Chat", on_click=dialog)
